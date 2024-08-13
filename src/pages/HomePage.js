c74c23e2c4ea82465aece5903b65f2437c00ba78
lechakrawarthy <chakravarthi1597@gmail.com>
2024-08-13 17:27:36 +0530

    chore: Update App.js and add HomePage component

---
+++ src/pages/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/Header';
+
+function HomePage() {
+  return (
+    <div>
+      <Header />
+      <main className="p-4">
+        <h2 className="text-xl font-semibold">Welcome to the Gitam GitHub Community!</h2>
+        {/* content */}
+      </main>
+    </div>
+  );
+}
+
+export default HomePage;
