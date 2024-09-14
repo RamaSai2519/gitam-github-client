3fc9516307c6cd9fd8e5244853060e4da353b2e7
Rama Sathya Sai <rchandik@gitam.in>
2024-09-14 19:02:49 +0530

    proxy

--- src/utils/axios_helper.js
+++ src/utils/axios_helper.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const MAIN_URL = "https://test.sukoonunlimited.com"
+const MAIN_URL = "https://proxy.sukoonunlimited.com"
 const DEV_URL = "http://localhost:8000"
 
 const BASE_URL = process.env.REACT_APP_ENV === 'main' ? MAIN_URL : DEV_URL;
